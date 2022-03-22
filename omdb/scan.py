7df835421967edb44b96f329c58713423f77f39d
vicch <vicch7@gmail.com>
2022-03-21 22:27:33 -0400

    Add OMDB script

---
+++ omdb/scan.py
@@ -0,0 +1,60 @@
+# This script reads movie data of given IMDB ID range from OMDB API and exports to CSV
+
+import sys
+import time
+import csv
+import requests
+
+BASE_URL = 'https://www.omdbapi.com/?apiKey=e1ce9b62&i='
+HEADERS = ['link', 'id', 'type', 'title', 'year', 'country', 'genre', 'director', 'rating', 'count', 'date']
+
+def scan_range(from_id, to_id):
+	file_name = '%s-%s.csv' % (from_id, to_id)
+	for id in range(from_id, to_id + 1):
+		movie_id = format_id(id)
+		with open(file_name, 'a', newline='') as file:
+			writer = csv.DictWriter(file, fieldnames=HEADERS, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+			writer.writerow(scan_movie(movie_id))
+
+def scan_movie(movie_id):
+	movie = None
+	while (movie is None):
+		movie = scan(movie_id)
+		time.sleep(5)
+	return movie
+
+def scan(movie_id):
+	try:
+		print('Scanning movie %s' % movie_id)
+
+		movie = {header: '' for header in HEADERS}
+		movie['id'] = movie_id
+		movie['date'] = time.strftime('%Y/%m/%d')
+
+		data = get_data(movie_id)
+		movie['type'] = data['Type']
+		movie['title'] = data['Title']
+		movie['year'] = data['Year']
+		movie['country'] = data['Country']
+		movie['genre'] = data['Genre']
+		movie['director'] = data['Director']
+		movie['rating'] = data['imdbRating']
+		movie['count'] = data['imdbVotes'].replace(',', '')
+
+		return movie
+
+	except Exception as e:
+		print(e)
+		return None
+
+def get_data(movie_id):
+	response = requests.get(BASE_URL + movie_id)
+	return response.json()
+
+def format_id(id):
+	return 'tt%07d' % id
+
+if __name__ == '__main__':
+	from_id = int(sys.argv[1])
+	to_id = int(sys.argv[2])
+	scan_range(from_id, to_id)
