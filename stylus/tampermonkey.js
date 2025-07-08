4a82cda1689fca70f75ff0dd3471f2183cbfc4bc
Victor Chen <vicch7@gmail.com>
2025-07-08 16:09:11 -0400

    Create tampermonkey.js

---
+++ stylus/tampermonkey.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Stylus
+// @namespace    http://tampermonkey.net/
+// @version      2023-12-13
+// @description  Stylesheet overrides
+// @author       You
+// @match        *://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @require      https://raw.githubusercontent.com/vicch/playground/dbc44cf80ad7d99c075485f4daa9e2b8415c75ee/stylus/stylus.js
+// @grant        GM_addStyle
+// @run-at       document-end
+// ==/UserScript==
