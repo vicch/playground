1d6a6bcafc02fed5ed60112f2c2fdbc46831ee4e
Victor Chen <vicch7@gmail.com>
2025-07-18 23:01:34 -0400

    Update stylus.js

--- stylus/stylus.js
+++ stylus/stylus.js
@@ -3,11 +3,11 @@
     @font-face {
       font-family: "MyFont";
       unicode-range: U+00-024F;
-      src: local("FFGoodProNarr-News");
       src: local("AvenirNextCondensed-Medium");
       src: local("BarlowCondensed-Regular");
       src: local("RobotoCondensed-Regular");
       src: local("Helvetica Condensed");
+      src: local("FFGoodProNarr-News");
     }
     @font-face {
       font-family: "MyFont";
@@ -27,11 +27,11 @@
     @font-face {
       font-family: "MyFontBold";
       unicode-range: U+00-024F;
-      src: local("FFGoodProNarr-Bold");
       src: local("AvenirNextCondensed-Bold");
       src: local("BarlowCondensed-Bold");
       src: local("RobotoCondensed-Bold");
       src: local("Helvetica Condensed Bold");
+      src: local("FFGoodProNarr-Bold");
     }
     @font-face {
       font-family: "MyFontBold";
