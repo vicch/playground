87d19b9def101eb9f79ee0ce4ad2a45f1a1ca7ec
Victor Chen <vicch7@gmail.com>
2026-06-17 21:34:28 -0400

    Update Stylus user script metadata and requirements
    Updated version and description for the Stylus user script. Changed the script requirements and execution timing.

--- stylus/tampermonkey.js
+++ stylus/tampermonkey.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Stylus
 // @namespace    http://tampermonkey.net/
-// @version      2023-12-13
-// @description  Stylesheet overrides
+// @version      2026-06-17
+// @description  Override page fonts with my custom font sets
 // @author       You
 // @match        *://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @require      https://raw.githubusercontent.com/vicch/playground/fde1334bf87e8833ea78a2ceb3fff4dcd31d053e/stylus/stylus.js
-// @grant        GM_addStyle
-// @run-at       document-end
+// @require      https://raw.githubusercontent.com/vicch/playground/refs/heads/master/stylus/stylus.js
+// @grant        none
+// @run-at       document-start
 // ==/UserScript==
 
 // ==UserScript==
